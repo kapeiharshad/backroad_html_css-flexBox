69638d00d52b9df4f52639e040cc48547db94c5e
kapeiharshad <kapeiharshad@gmail.com>
2020-03-08 00:23:54 +0530

    nav position remove

--- app.js
+++ app.js
@@ -1,21 +1,58 @@
+/** @format */
+
 // ************** set date *****************
-const date = document.getElementById("date").innerHTML=new Date().getFullYear();
+const date = (document.getElementById(
+  "date"
+).innerHTML = new Date().getFullYear());
 
 // ************** nav toggle **************
-const navBtn =document.getElementById("nav-toggle");
-const links =document.getElementById("nav-options");
+const navBtn = document.getElementById("nav-toggle");
+const links = document.getElementById("nav-options");
 
-navBtn.addEventListener("click",()=>{
-    links.classList.toggle("show-nav-options")
-})
+navBtn.addEventListener("click", () => {
+  links.classList.toggle("show-nav-options");
+});
 
 // ***************** fixed nav *************
-const navbar =document.querySelector(".navbar");
+const navbar = document.querySelector(".navbar");
 
-window.addEventListener("scroll",()=>{
-    if (window.pageYOffset > 16) {
-        navbar.classList.add("fixed")
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 16) {
+    navbar.classList.add("fixed");
+  } else {
+    navbar.classList.remove("fixed");
+  }
+});
+
+// **************** smooth scroll ***********
+
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach(link => {
+  link.addEventListener("click", e => {
+    e.preventDefault();
+    links.classList.remove("show-nav-options");
+    const id = e.target.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    let position;
+    if (navbar.classList.contains("fixed")) {
+      position = element.offsetTop - 51;
     } else {
-        navbar.classList.remove("fixed")
+      position = element.offsetTop - 100;
     }
-})
+
+    if (window.innerWidth < 992) {
+      if (navbar.classList.contains("fixed")) {
+        position = element.offsetTop - 51;
+      } else {
+        position = element.offsetTop - 321 - 51;
+      }
+    }
+
+    window.scrollTo({
+      left: 0,
+      top: position,
+      behavior: "smooth"
+    });
+  });
+});
